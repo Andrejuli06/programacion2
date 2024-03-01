f7511aecf04a39bf242f6ef54ef1c5a9ec717c0d
AndreaC <acasadiego979@gmail.com>
2024-03-01 12:03:25 -0500

    creacion de paquetes, getters ny setters con sobre carga del constructor

--- poo/src/main/java/com/mycompany/poo/Poo.java
+++ poo/src/main/java/com/mycompany/poo/Poo.java
@@ -1,31 +1,15 @@
-
 package com.mycompany.poo;
+import characters.Character;
+
 public class Poo {
 
     public static void main(String[] args) {
-        Character homero= new Character("Homero","Inspectpr de seguridad",33,"A la grande le puse cuca");
+        Character homero = new Character("Homero", "Inspector de seguridad", 33, "A la grande le puse cuca");
         homero.greeting();
-        System.out.println(homero.age);
+       System.out.println(homero.getAge());
         Character bart = new Character("bartolomeo", "vago", 10, "Ay caramba");
         bart.greeting();
-        
-    }
-     static class Character  {
-        String name;
-        String occupation;
-        int age;
-        String phrase;
 
-        public Character(String name, String occupation, int age, String phrase) {
-            this.name = name;
-            this.occupation = occupation;
-            this.age = age;
-            this.phrase = phrase;
-        }
-       
-        void greeting() {
-            System.out.println(this.phrase);
-        }
-        
     }
+
 }
