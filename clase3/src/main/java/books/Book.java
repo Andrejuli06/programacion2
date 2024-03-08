52ac4e8d509cb1c486c296fbcb6441931424be51
Andrejuli06 <acasadiego979@gmail.com>
2024-03-08 11:57:11 -0500

    avance del ejercicio en clase

--- clase3/src/main/java/books/Book.java
+++ clase3/src/main/java/books/Book.java
@@ -1,29 +1,54 @@
-
 package books;
 
 public class Book {
-  String title;
-  String gender;
-  int id;
-  int quanty;
-  String loanDate;
-  String returnDante; 
-  boolean available;
 
-    public Book(String title, String gender, int id, int quanty) {
+    String title;
+    String gender;
+    String author;
+    int id;
+    int quanty;
+    String loanDate;
+    String returnDante;
+    String user;
+    boolean available;
+    boolean lend;
+
+    public Book(String title, String gender, String author, int id, int quanty, String loanDate, String returnDante, String user, boolean available, boolean lend) {
         this.title = title;
         this.gender = gender;
+        this.author = author;
         this.id = id;
         this.quanty = quanty;
-       this.available=true;
+        this.loanDate = loanDate;
+        this.returnDante = returnDante;
+        this.user = user;
+        this.available = available;
+        this.lend = lend;
     }
-  
-  public void LendBook(){
-      this.available=false;
-  }
-  void returnbook(){
-      this.available=true;
-  }
+    
+
+    public Book(String title, boolean lend) {
+        this.title = title;
+        this.lend = lend;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isLend() {
+        return lend;
+    }
+
+   
+
+    public void LendBook() {
+        this.available = false;
+    }
+
+    public void returnbook() {
+        this.available = true;
+    }
 
     public boolean isAvailable() {
         return available;
---
+++ clase3/src/main/java/boroowed_books/BorrowedBooks.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boroowed_books;
+
+/**
+ *
+ * @author SCIS2PC17
+ */
+public class BorrowedBooks {
+    
+}
